b13f5b24813b924bad326118f439f5f215f999ad
Anderson Chen <twan0817@gmail.com>
2023-03-05 22:50:24 +0800

    feat: add speakers

--- public/constant/speakers.ts
+++ public/constant/speakers.ts
@@ -4,14 +4,14 @@ type SpeakerProps = {
 };
 
 const speakers: SpeakerProps[] = [
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
-  // { name: "Name", company: "Company" },
+  { name: "Hsiao-Wei Wang", company: "Ethereum Foundation",  },
+  { name: "Cathie", company: "Ethereum Foundation" },
+  { name: "Vivian", company: "Ethereum Foundation" },
+  { name: "Wanseob", company: "Ethereum Foundation" },
+  { name: "guiltygyoza", company: "Topology" },
+  { name: "Daniele Ugolini", company: "Rysk Finance" },
+  { name: "Anderson Chen", company: "Diamond Protocol" },
+  { name: "Chris Tam", company: "BTQ" },
   // { name: "Name", company: "Company" },
   // { name: "Name", company: "Company" },
 ];
