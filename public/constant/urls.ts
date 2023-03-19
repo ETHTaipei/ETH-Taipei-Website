34a14089662c9b864cea6153067ebb547e8136c2
Anton Cheng <antonassocareer@gmail.com>
2023-03-19 10:22:46 +0800

    build: add grenade, blockbooster as partners (#54)
    * build: add grenade, abmedia, blocktempo
    
    * chore: add block booster
    
    * chore: remove

--- public/constant/urls.ts
+++ public/constant/urls.ts
@@ -28,4 +28,8 @@ export const ethereumOrgUrl = "https://ethereum.org/en/";
 export const taikaiUrl = "https://taikai.network/";
 export const unlockUrl = "https://unlock-protocol.com/";
 
+export const grenadeUrl = 'https://www.grenade.tw/'
+export const abmediaUrl = 'https://abmedia.io/'
+export const blocktempoUrl = 'https://www.blocktempo.com/'
+export const blockBoosterUrl = 'https://twitter.com/0xBlockBooster'
 export const visaUrl = "https://www.boca.gov.tw/cp-149-4486-7785a-2.html";
