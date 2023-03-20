63d894bb1c57eaeca0d678517dba621d70cab23c
Annie <77234273+AnnieChi1020@users.noreply.github.com>
2023-03-20 11:02:20 +0800

    feat: add social links in footer (#56)

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,12 +2,35 @@ import React from "react";
 import styled from "styled-components";
 import Colors from "@/styles/colors";
 import t from "@/public/constant/content";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { openNewTab } from "@/public/utils/ common";
+import { discordUrl, telegramUrl, twitterUrl } from "@/public/constant/urls";
+import {
+  faTwitter,
+  faTelegram,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
+  const handleSocialMediaOnClick = (url: string) => {
+    openNewTab(url);
+  };
+
   return (
     <Container>
       <MainFooter>
         <EventName>{t.common.ethTaipei}</EventName>
+        <SocialMediaContainer>
+          <IconButton onClick={() => handleSocialMediaOnClick(twitterUrl)}>
+            <Icon icon={faTwitter} />
+          </IconButton>
+          <IconButton onClick={() => handleSocialMediaOnClick(telegramUrl)}>
+            <Icon icon={faTelegram} />
+          </IconButton>
+          <IconButton onClick={() => handleSocialMediaOnClick(discordUrl)}>
+            <Icon icon={faDiscord} />
+          </IconButton>
+        </SocialMediaContainer>
       </MainFooter>
     </Container>
   );
@@ -26,6 +49,11 @@ const MainFooter = styled.div`
   max-width: 1440px;
   padding: 12px 48px 12px 32px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
   @media (max-width: 992px) {
     justify-content: center;
   }
@@ -38,4 +66,29 @@ const EventName = styled.h4`
     font-size: 12px;
     line-height: 18px;
   }
+`;
+
+const SocialMediaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  @media (max-width: 992px) {
+    margin-top: 20px;
+  }
+`;
+
+const IconButton = styled.button`
+  padding: 4px;
+  cursor: pointer;
+  color: ${Colors.pennBlue};
+  :hover {
+    color: ${Colors.aero};
+  }
+  :active {
+    transform: scale(0.9);
+  }
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+  font-size: 24px;
 `;
