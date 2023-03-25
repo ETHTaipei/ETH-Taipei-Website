cb0aa20bcabe011079e40ada22fe9b0dcc89b73a
Anton Cheng <antonassocareer@gmail.com>
2023-03-25 22:38:01 +0800

    feat: embed video

--- components/HomePage/Banner.tsx
+++ components/HomePage/Banner.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 
 import banner from "@/public/images/vertical-transparent.png";
 import t from "@/public/constant/content";
-import bgImage from "@/public/images/eth-taipei-banner-background-2.png";
 import { hackathonUrl } from "@/public/constant/urls";
 import gtagReportConversion from "@/public/utils/gtag";
 import { openNewTab } from "@/public/utils/common";
@@ -27,7 +26,13 @@ const Banner = () => {
 
   return (
     <Container>
-      <Image src={bgImage} fill quality={100} alt="bgImage" />
+      <VideoWrapper>
+        <iframe
+          src="https://www.youtube.com/embed/1aem3HuYIzM?autoplay=1&mute=1&loop=1&playlist=1aem3HuYIzM&controls=0&showinfo=0&autohide=1&modestbranding=1&rel=0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen={true}
+        />
+      </VideoWrapper>
       <ImageContainer>
         <Image src={banner} alt="logo" fill />
       </ImageContainer>
@@ -47,6 +52,30 @@ const Banner = () => {
 
 export default Banner;
 
+const VideoWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: -1;
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    filter: grayscale(0.5) brightness(1.2);
+    @media (max-width: 300px) {
+      width: auto;
+      height: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+`;
+
 const Container = styled.div`
   min-height: calc(100vh - 70px);
   width: 100%;
@@ -54,7 +83,7 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  @media (max-width: 992px) {
+  @media (max-width: 300px) {
     min-height: calc(100vh - 60px);
   }
   position: relative;
@@ -70,15 +99,30 @@ const ImageContainer = styled.div`
   position: relative;
   width: 400px;
   height: 400px;
-  > image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > img {
     object-fit: contain;
     object-position: center;
   }
   @media (max-width: 768px) {
-    width: 240px;
-    height: 240px;
+    width: 160px;
+    height: 160px;
+  }
+
+  ::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 20px;
+    z-index: -1;
   }
 `;
+
 
 const ActivitiesContainer = styled.div`
   margin-top: 16px;
