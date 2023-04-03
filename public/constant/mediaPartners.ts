8560f1ce473c7080acaac273641489b4b9fef556
Anderson Chen <twan0817@gmail.com>
2023-04-03 09:44:23 +0800

    feat: add media partners (#91)

--- public/constant/mediaPartners.ts
+++ public/constant/mediaPartners.ts
@@ -3,6 +3,9 @@ import { web3plusUrl, cryptoCityUrl, tbaUrl } from "./urls";
 import web3Plus from "@/public/images/web3Plus.png";
 import cryptoCity from "@/public/images/cryptoCity.png";
 import tba from "@/public/images/tba.png";
+import coinnewshk from "@/public/images/coinnewshk.png";
+import blockbeats from "@/public/images/blockbeats.png";
+import nineup from "@/public/images/nineup.png";
 
 import { StaticImageData } from "next/image";
 
@@ -17,6 +20,9 @@ const mediaPartners: MediaPartnerProps[] = [
   { name: "Web3+", url: web3plusUrl, logo: web3Plus },
   { name: "Crypto City", url: cryptoCityUrl, logo: cryptoCity },
   { name: "Taiwan Blockchain Academia", url: tbaUrl, logo: tba },
+  { name: "9up.io", url: web3plusUrl, logo: nineup },
+  { name: "律動", url: cryptoCityUrl, logo: blockbeats },
+  { name: "幣訊", url: tbaUrl, logo: coinnewshk },
 ];
 
 export default mediaPartners;
