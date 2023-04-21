fac82b30181d001b52851ff84443a3cd9c9738d7
Anton Cheng <antonassocareer@gmail.com>
2023-04-21 14:45:10 +0800

    chore: remove countdown (#163)
    * chore: remove countdown
    
    * chore: fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Banner from "@/components/HomePage/Banner";
 import Introduction from "@/components/HomePage/Introduction";
-import CountdownTimer from "@/components/HomePage/CountDownTimer";
 import Activities from "@/components/HomePage/Activities";
 import ApplicationForm from "@/components/HomePage/ApplicationForm";
 import Venue from "@/components/HomePage/Venue";
@@ -16,7 +15,6 @@ const Home = () => {
     <div>
       <Banner />
       <Introduction />
-      <CountdownTimer />
       <Activities />
       <ApplicationForm />
       <Venue />
