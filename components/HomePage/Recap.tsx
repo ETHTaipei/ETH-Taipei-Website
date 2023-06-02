470b50a59b93bf50674db045fc5eda6ae4386ea6
Anton Cheng <antonassocareer@gmail.com>
2023-06-02 09:41:33 +0200

    build: recap

--- components/HomePage/Recap.tsx
+++ components/HomePage/Recap.tsx
@@ -1,7 +1,6 @@
 import Colors from "@/styles/colors";
 import React from "react";
 import styled from "styled-components";
-import { openNewTab } from "@/public/utils/common";
 import t from "@/public/constant/content";
 
 const Recap = () => {
@@ -9,7 +8,7 @@ const Recap = () => {
     <Container>
       <MainContent>
         <Title>{t.homepage.recap}</Title>
-        <iframe width="900" height="506" src="https://www.youtube.com/embed/G7uA9RNQ8FA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="900" height="506" src="https://www.youtube.com/embed/G7uA9RNQ8FA" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={true}></iframe>
       </MainContent>
     </Container>
   );
@@ -41,22 +40,4 @@ const MainContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-
-const Application = styled.button`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: ${Colors.btnBlue};
-  border-radius: 8px;
-  padding: 20px 40px;
-  cursor: pointer;
-  box-shadow: 0 4px 8px 0 rgba(36, 62, 81, 0.2),
-    0 6px 12px 0 rgba(63, 83, 114, 0.19);
-  :active {
-    transform: scale(0.99);
-  }
 `;
