fb0b436ccebd5aafc726f4b5aa5950164ff8c8f7
tienshaoku <tienshaoku@gmail.com>
2025-03-26 07:47:00 +0800

    chore: remove unused fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -62,10 +62,12 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter:wght@300&display=swap" rel="stylesheet"/>
-          <link href="https://fonts.googleapis.com/css2?family=Rammetto+One&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
